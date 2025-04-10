fb15e5380d1f9e55e1b3770e3650cff3d61191e8
lubuntum <lubuntum@gmail.com>
2025-04-10 14:34:25 +0700

    Обновленный зедеро + стили размер футера

--- src/components/header/HeaderComponent.jsx
+++ src/components/header/HeaderComponent.jsx
@@ -3,22 +3,28 @@ import "../../styles/header.css"
 import { useAuth } from "../../services/auth/AuthProvider"
 
 import logo from "../../res/images/crm-logo.jpg"
-
+import { Route, useNavigate } from "react-router-dom"
+import { ROUTES } from "../../routes"
+import simpleLogo from "../../res/images/simple_logo.svg"
 export const HeaderComponent = () => {
     const { logout, checkAuth } = useAuth()
+    const navigation = useNavigate()
+
     return (
         <header>
-            <div className="logo">
-                <img src={logo} alt="logo"></img>
+            <div onClick={() => navigation(ROUTES.HOME)} className="logo">
+                <img src={simpleLogo} alt="logo"></img>
             </div>
 
             <div className="nav">
-                <a>Главная</a>
-                {checkAuth && <a>Аккаунт</a>}
+                <a href={ROUTES.HOME}>Главная</a>
+                {checkAuth() && <a href={ROUTES.ACCOUNT}>Аккаунт</a>}
+                {checkAuth() && <a href={ROUTES.CREATE_ORDER}>Создать заказ</a>}
+                {checkAuth() && <a href={ROUTES.ORDERS}>Заказы</a>}
             </div>
 
             <div className="login">
-                {checkAuth ? <a onClick={logout}>Выход</a> : <a onClick={()=>{}}>Войти</a>}
+                {checkAuth() ? <a onClick={logout}>Выход</a> : <a onClick={() => navigation(ROUTES.AUTH)}>Войти</a>}
             </div>
         </header>
     )
