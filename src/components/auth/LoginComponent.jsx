aa7a8c278daa3641c0cebbdab4be6967dd9ade10
lubuntum <lubuntum@gmail.com>
2025-04-11 13:17:07 +0700

    По какой то причине не было функции логина в аккаунт, добавлено

--- src/components/auth/LoginComponent.jsx
+++ src/components/auth/LoginComponent.jsx
@@ -5,16 +5,16 @@ import { useAuth } from "../../services/auth/AuthProvider"
 import { authComponentsKeys } from "./AuthPage"
 import "../../styles/statuses/statuses.css"
 import { ReactComponent as FaceIcon } from "../../res/icons/face_24dp_E8EAED_FILL0_wght400_GRAD0_opsz24.svg"
-
 const LoginComponent = ({setCurrentComponent}) => {
     const [status, setStatus] = useState(STATUSES.IDLE)
     const [email, setEmail] = useState(null)
     const [pass, setPass] = useState(null)
     const {login} = useAuth()
-
+    
     const loginHandler = async () => {
         try {
             const response = await loginRequest(email, pass)
+            login(response.data)
         } catch(err) {
             setStatus(STATUSES.ERROR)
         }
