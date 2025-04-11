749845e436d538885caf3a5ed789a6e4c3537d16
lubuntum <lubuntum@gmail.com>
2025-04-11 13:17:59 +0700

    Установлен пакет react-table + первая таблица для заказов в тестовом режиме

--- src/components/order/orders-list/OrdersList.jsx
+++ src/components/order/orders-list/OrdersList.jsx
@@ -1,10 +1,37 @@
-
-export const OrdersList = () => {
-
+import { useTable } from "react-table"
+import "../../../styles/orders/orders_list/orders_list.css"
+export const OrdersList = ({columns, data}) => {
+    const {getTableProps, getTableBodyProps,headerGroups, rows, prepareRow} = useTable({columns, data})
     return(
         <>
-        <div className="contentWrapper">
-            <p>Orders list</p> 
+        <div className="tableWrapper">
+            <table className="ordersList">
+                <thead>
+                    {headerGroups.map(headerGroup => (
+                        <tr {...headerGroup.getHeaderGroupProps()}>
+                            {headerGroup.headers.map(column => (
+                                <th {...column.getHeaderProps()}>
+                                    {column.render('Header')}
+                                </th>
+                            ))}
+                        </tr>
+                    ))}
+                </thead>
+                <tbody {...getTableProps()}>
+                    {rows.map(row => {
+                        prepareRow(row)
+                        return (
+                            <tr {...row.getRowProps()} onClick={()=>{console.log(row.getRowProps())}}>
+                                {row.cells.map(cell => (
+                                    <td {... cell.getCellProps()}>
+                                        {cell.render('Cell')}
+                                    </td>
+                                ))}
+                            </tr>
+                        )
+                    })}
+                </tbody>
+            </table>
         </div>
         </>
     )
--- src/components/order/orders-list/OrdersPage.jsx
+++ src/components/order/orders-list/OrdersPage.jsx
@@ -1,13 +1,50 @@
+import { useMemo } from "react"
 import { FooterComponent } from "../../footer/FooterComponent"
 import { HeaderComponent } from "../../header/HeaderComponent"
 import { OrdersList } from "./OrdersList"
 
 export const OrdersPage = () => {
-
+    const columns = useMemo(() => [
+        {
+            Header: "Имя",
+            accessor: "fullName"
+        },
+        {
+            Header: "Почта",
+            accessor: "clientEmail"
+        },
+        {
+            Header: "Номер",
+            accessor: "clientPhone"
+        },
+        {
+            Header: "Адрес",
+            accessor: "address"
+        }
+    ])
+    const tempData = [{
+        fullName: "Bolshakov Pavel Dmitrievich",
+        clientEmail: "Test@gmail.com",
+        clientPhone: "1 123 313 12 43", 
+        address: "Some addres st12 house25 NW"
+    },
+    {
+        fullName: "Ivanov Ivan Ivanovich",
+        clientEmail: "Test@gmail.com",
+        clientPhone: "1 123 313 12 43", 
+        address: "Some addres st12 house25 NW"
+    },
+    {
+        fullName: "Tester Test Testerov",
+        clientEmail: "Test@gmail.com",
+        clientPhone: "1 123 313 12 43", 
+        address: "Some addres st12 house25 NW"
+    },
+]
     return (
         <>
             <HeaderComponent />
-            <OrdersList />
+            <OrdersList data={tempData} columns={columns} />
             <FooterComponent/>
         </>
     )
