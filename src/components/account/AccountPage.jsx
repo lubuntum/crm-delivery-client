d048b0ba726602f703bd2069d5430f7c546f2617
lubuntum <lubuntum@gmail.com>
2025-04-11 18:29:49 +0700

    Обработка ошибки при загрузке данных аккаунта

--- src/components/account/AccountPage.jsx
+++ src/components/account/AccountPage.jsx
@@ -7,13 +7,20 @@ import { useEffect, useState } from "react"
 import { getAccountDataRequest } from "../../services/api/accountApi"
 import { useAuth } from "../../services/auth/AuthProvider"
 import Cookies from "js-cookie"
+import { STATUSES } from "../../statuses"
 export const AccountPage = () => {
     const {getToken} = useAuth()
     const [accountData, setAccountData] = useState(null)
+    const [status, setStatus] = useState(STATUSES.IDLE)
     useEffect(()=>{
         const getAccountData = async () => {
-            const response = await getAccountDataRequest(getToken())
-            setAccountData(response.data)
+            try {
+                const response = await getAccountDataRequest(getToken())
+                setAccountData(response.data)
+            } catch(err) {
+                setStatus(STATUSES.ERROR)
+            }
+            
         }
         getAccountData()
     }, [])
