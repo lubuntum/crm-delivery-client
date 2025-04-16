7126b2922e8d717b0bfcdf99ce1189921d405d08
lubuntum <lubuntum@gmail.com>
2025-04-16 16:41:06 +0700

    импорт css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import './styles/ui_elements/buttons.css'
 import './styles/ui_elements/inputs.css'
+import './styles/ui_elements/loading_bar.css'
 import {Route, Routes, Navigate, BrowserRouter} from 'react-router-dom'
 import { ROUTES } from './routes';
 import { AuthPage } from './components/auth/AuthPage';
