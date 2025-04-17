8b5f4f7d4a72bf7e2db0bf1f6b81a4c874faef3c
lubuntum <lubuntum@gmail.com>
2025-04-17 17:53:24 +0700

    Новые API Urls

--- src/services/api/urls.js
+++ src/services/api/urls.js
@@ -2,8 +2,14 @@ export const SERVER_URL = "http://192.168.11.10:8080"
 export const REGISTER = "/api/auth/register"
 export const LOGIN = "/api/auth/login"
 export const ACCOUNT_DATA = "/api/auth/account-data"
+
 export const CREATE_ORDER = "/api/orders/create-order"
 export const CHANGE_ORDER_STATUS = "/api/orders/change-order-status"
 export const GET_ORDER_BY_ID = "/api/orders/order"
+
+export const CREATE_ORDER_PICKUP = "/api/orders-pickup/create"
+export const GET_ORDER_PICKUP_BY_ID = "/api/orders-pickup/order-pickup"
+export const GET_ORDER_PICKUP_BY_ORDER_ID = "/api/orders-pickup/order-pickup/by-order-id"
+
 export const ORGANIZATION_ORDERS = "/api/organization/orders"
 export const BARREL_COMPONENTS = "/api/barrel/components"
