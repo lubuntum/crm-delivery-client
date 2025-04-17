f079e1d3482b3eef26150ef5785315fdd6b1bf03
lubuntum <lubuntum@gmail.com>
2025-04-17 17:57:22 +0700

    Реализована форма для работы с забором заказа от клиента

--- src/components/order/pickup-order/OrderPickupForm.jsx
+++ src/components/order/pickup-order/OrderPickupForm.jsx
@@ -6,12 +6,18 @@ import { ORDER_STATUSES, STATUSES } from "../../../statuses"
 import { changeOrderStatusRequest, getOrderByIdRequest } from "../../../services/api/orderApi"
 import { useAuth } from "../../../services/auth/AuthProvider"
 import { ROUTES } from "../../../routes"
+import { DIGIT_REGEX } from "../../../services/validation/validationRegexes"
+import { createOrderPickupRequest, getOrderPickupByOrderIdRequest } from "../../../services/api/orderPickupApi"
 export const OrderPickupForm = () => {
     const location = useLocation()
     const navigate = useNavigate()
     const {getToken} = useAuth()
     const [order, setOrder] = useState()
-
+    const [orderPickup, setOrderPickup] = useState({
+        itemsCount: "",
+        comment: "",
+        orderId: null
+    })
     const [status, setStatus] = useState(STATUSES.IDLE)
     useEffect(()=> {
         const param = new URLSearchParams(window.location.search)
@@ -22,25 +28,59 @@ export const OrderPickupForm = () => {
             try {
                 setStatus(STATUSES.LOADING)
                 const response = await getOrderByIdRequest(param.get("id"), getToken())
-                setOrder(response.data)
+                const orderTemp = response.data
+                setOrder(orderTemp)
+                //Если статус Taken это значит что уже есть orderPickup и можно его запросить
+                if (orderTemp.status !== ORDER_STATUSES.PICKED && orderTemp.status !== ORDER_STATUSES.CREATED)
+                    await getOrderPickupByOrderId(orderTemp.id)
                 setStatus(STATUSES.IDLE)
             } catch(err) {
                 setStatus(STATUSES.ERROR)
             }
         }
+        const getOrderPickupByOrderId = async (orderId) => {
+            const response = await getOrderPickupByOrderIdRequest(getToken(), orderId)
+            setOrderPickup(response.data)
+        }
         getOrderById()
     }, [])
 
     const changeOrderStatusHandler = async (orderStatus) => {
+        //Если заказ не имеет текущих статусов, здесь с ним работа закончена
+        if (order.status !== ORDER_STATUSES.CREATED && order.status !== ORDER_STATUSES.PICKED 
+            && order.status !== ORDER_STATUSES.TAKEN) return
         try {
             const response = await changeOrderStatusRequest(order.id, orderStatus, getToken())
-            setOrder(prev => ({...order, status: response.data}))
+            setOrder(prev => ({...prev, status: response.data}))
+            setStatus(STATUSES.IDLE)
         } catch (err) {
             setStatus(STATUSES.ERROR)
             console.error(err)
         }
     }
-
+    const createOrderPickupHandler = async () => {
+        if (order?.status !== ORDER_STATUSES.PICKED) return 
+        if (!DIGIT_REGEX.test(orderPickup.itemsCount)) {
+            setStatus(STATUSES.VALIDATION_ERROR)
+            return
+        }
+        try {
+            const orderPickupData = orderPickup
+            orderPickupData.orderId = order.id
+            await createOrderPickupRequest(getToken(), orderPickupData)
+            await changeOrderStatusHandler(ORDER_STATUSES.TAKEN)
+            setStatus(STATUSES.SUCCESS)
+        } catch(err) {
+            setStatus(STATUSES.ERROR)
+        }
+    }
+    const orderPickupHandler = async (e) => {
+        const {name, value} = e.target
+        setOrderPickup((prevData) => ({
+            ...prevData,
+            [name]: value
+        }))
+    }
     return(
         <>
             <div className="contentWrapper">
@@ -54,14 +94,16 @@ export const OrderPickupForm = () => {
                 {order && order.status !== ORDER_STATUSES.CREATED &&
                     <div className="formWrapper">
                         <div className="form">
-                            <div className="orderTitle">
-                                Забрать заказ
+                            <div className="formTitle">
+                            <p>Забрать заказ</p>
+                            {status === STATUSES.VALIDATION_ERROR && <p className="errorText">Формат некоторых полей неверен</p>}
+                            {status === STATUSES.SUCCESS && <p className="successText">Заказ успешно забран</p>}
                             </div>
                             <div className="formInputs">
-                                <input type="text" placeholder="Количество" />
-                                <textarea placeholder="Комментарий"/>
-                                <button className={order?.status !== ORDER_STATUSES.PICKED && "blocked"}>Подтвердить</button>
-                                <button className={order?.status !== ORDER_STATUSES.TAKEN && "blocked"}>Доставлено</button>
+                                <input type="number" step={1} min={1} required name = "itemsCount" placeholder="Количество" value={orderPickup.itemsCount} onChange={orderPickupHandler} />
+                                <textarea name="comment" placeholder="Комментарий" value={orderPickup.comment} onChange={orderPickupHandler}/>
+                                <button className={order?.status !== ORDER_STATUSES.PICKED && "blocked"} onClick={createOrderPickupHandler}>Забрать</button>
+                                <button className={order?.status !== ORDER_STATUSES.TAKEN && "blocked"} onClick={() => changeOrderStatusHandler(ORDER_STATUSES.INSPECTION)}>Доставлено</button>
                             </div>
                         </div>
                     </div>
