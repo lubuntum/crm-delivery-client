bd2e6a835ffeedf50570b3f33466af147874833d
lubuntum <lubuntum@gmail.com>
2025-04-22 09:50:56 +0700

    Таблица всех элементов заказа + компонент для инспекции заказа

--- src/components/order/inspection/ItemList.jsx
+++ src/components/order/inspection/ItemList.jsx
@@ -1,29 +1,35 @@
+import { useState } from "react"
 import "../../../styles/orders/orders_list/orders_list.css"
 export const ItemList = ({orderItems}) => {
     const tableHeaderData = ['Материал', 'Размер', 'Цена']
+    const [isReady, setIsReady] = useState(false)
     return (
         <>  
-            <table className="ordersList">
-                <thead>
-                    <tr>
-                        {tableHeaderData.map(header => (
-                            <th>
-                                <p>{header}</p>
-                            </th>
-                        ))}
-                    </tr>
-                </thead>
-                <tbody>
-                    {orderItems && orderItems.map(item => (
+        <div className="itemsWrapper">
+            <table>
+                    <thead>
                         <tr>
-                            <th>{item.material}</th>
-                            <th>{item.size}</th>
-                            <th>{item.price}</th>
-                            <th><input style={{width:17, height: 17}} type="checkbox" /></th>
+                            {tableHeaderData.map(header => (
+                                <th>
+                                    <p>{header}</p>
+                                </th>
+                            ))}
                         </tr>
-                    ))}
-                </tbody>
-            </table>
+                    </thead>
+                    <tbody>
+                        {orderItems && orderItems.map(item => (
+                            <tr>
+                                <th>{item.materialName}</th>
+                                <th>{item.size}</th>
+                                <th>{item.price}</th>
+                                <th><input style={{width:17, height: 17}} type="checkbox" /></th>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+                {orderItems && <button className={!isReady && "blocked"}>Завершить проверку</button>}
+        </div>
+            
         </>
     )
 }
--- src/components/order/inspection/OrderInspection.jsx
+++ src/components/order/inspection/OrderInspection.jsx
@@ -1,10 +1,12 @@
-import { useEffect, useState } from "react"
+import { useEffect, useRef, useState } from "react"
 import { AddItemForm } from "./AddItemForm"
 import { ItemList } from "./ItemList"
 import { ORDER_STATUSES, STATUSES } from "../../../statuses"
 import { useNavigate } from "react-router-dom"
 import { getOrderByIdRequest } from "../../../services/api/orderApi"
 import { useAuth } from "../../../services/auth/AuthProvider"
+import { getMaterialByOrganizationId } from "../../../services/api/materialsApi"
+import { getItemsByOrderIdRequest } from "../../../services/api/itemApi"
 
 
 export const OrderInspection = () => {
@@ -12,27 +14,32 @@ export const OrderInspection = () => {
     const [order, setOrder] =  useState(null)
     const navigate = useNavigate()
     const {getToken} = useAuth()
-    //fetch from server later
-    const testItems = [
-        {material: "Хлопоп", size: "135", price: "2500"},
-        {material: "Шерсть", size: "100", price: "3500"},
-        {material: "Полиэстр", size: "25", price: "1200"},
-        {material: "Полиамид", size: "125", price: "3500"}
-    ]
+
+    const [items, setItems] = useState([{materialName: "Хлопоп", size: "135", price: "2500"},
+        {materialName: "Шерсть", size: "100", price: "3500"},
+        {materialName: "Полиэстр", size: "25", price: "1200"},
+        {materialName: "Полиамид", size: "125", price: "3500"}])
     useEffect(()=> {
         const param = new URLSearchParams(window.location.search)
         if (!param.get("id")) navigate(-1)
-        const getOrderById = async(id) => {
-            try {
-                setStatus(STATUSES.LOADING)
-                const response = await getOrderByIdRequest(param.get("id"), getToken())
-                setOrder(response.data)
-                setStatus(STATUSES.IDLE)
-            } catch(err) {
-                setStatus(STATUSES.ERROR)
-            }
+        const getOrderById = async() => {
+            setStatus(STATUSES.LOADING)
+            const response = await getOrderByIdRequest(param.get("id"), getToken())
+            setOrder(response.data)
+            setStatus(STATUSES.IDLE)
         }
-        getOrderById()
+        const getItemsByOrderId = async () => {
+            const response = await getItemsByOrderIdRequest(param.get("id"), getToken())
+            setItems(response.data)
+        }
+        try {
+            getOrderById()
+            getItemsByOrderId()
+        } catch(err) {
+            console.error(err)
+            setStatus(STATUSES.ERROR)
+        }
+        
     }, [])
     const checkOrderForPrevStatus = () => {
         return (order && (order.status === ORDER_STATUSES.CREATED || 
@@ -41,15 +48,18 @@ export const OrderInspection = () => {
     }
     return (
         <> 
-            <div className="contentWrapper">
-                {status === STATUSES.LOADING && <div className="loadingBar"> </div>}
-                {checkOrderForPrevStatus() && <p>Заказ пока не готов к инспекции</p> }
-                {(order && !checkOrderForPrevStatus()) && 
-                    <>
-                        <AddItemForm />
-                        <ItemList orderItems = {testItems} />
-                    </>}
-            </div>
+        <div className="inspectionContainer">
+            <div className="inspectionContentWrapper">
+                    {status === STATUSES.LOADING && <div className="loadingBar"> </div>}
+                    {checkOrderForPrevStatus() && <p>Заказ пока не готов к инспекции</p> }
+                    {(order && !checkOrderForPrevStatus()) && 
+                        <>
+                            <AddItemForm setItems={setItems}/>
+                            <ItemList orderItems = {items} />
+                        </>}
+                </div>
+        </div>
+            
         </>
     )
 }
