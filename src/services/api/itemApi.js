0bbf35240f897c9dac11eb89d1db140826141a79
lubuntum <lubuntum@gmail.com>
2025-04-25 12:46:52 +0700

    API для обновления готовности для позиции

--- src/services/api/itemApi.js
+++ src/services/api/itemApi.js
@@ -1,5 +1,5 @@
 import axios from "axios"
-import { CREATE_ITEM_FOR_ORDER, GET_ITEMS_BY_ORDER_ID, SERVER_URL } from "./urls"
+import { CHANGE_ITEM_READY_STATE, CREATE_ITEM_FOR_ORDER, GET_ITEMS_BY_ORDER_ID, SERVER_URL } from "./urls"
 
 export const createItemRequest = async (item, token) => {
     return await axios.post(`${SERVER_URL}${CREATE_ITEM_FOR_ORDER}`, item, {
@@ -10,5 +10,11 @@ export const createItemRequest = async (item, token) => {
 export const getItemsByOrderIdRequest = async (orderId, token) => {
     return await axios.get(`${SERVER_URL}${GET_ITEMS_BY_ORDER_ID}/${orderId}`, {
         headers: {Authorization: token}
+    })
+}
+export const updateItemReadyStateRequest = async (id, isReady, token) => {
+    console.log(id, isReady, token)
+    return await axios.patch(`${SERVER_URL}${CHANGE_ITEM_READY_STATE}?id=${id}&isReady=${isReady}`, {},
+        {headers: {Authorization: token}
     })
 }
