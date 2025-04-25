ada9e350567d041b69e6e5621a1478909260565b
lubuntum <lubuntum@gmail.com>
2025-04-25 12:44:29 +0700

    Полностью реализован функционал работы со списком позиций, их добавление, выполнение, завершение инспекции, фидбек статуса заказа

--- src/components/order/inspection/ItemList.jsx
+++ src/components/order/inspection/ItemList.jsx
@@ -1,17 +1,49 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import "../../../styles/orders/orders_list/orders_list.css"
-export const ItemList = ({orderItems, setItem}) => {
+import { updateItemReadyStateRequest } from "../../../services/api/itemApi"
+import { useAuth } from "../../../services/auth/AuthProvider"
+import { ORDER_STATUSES } from "../../../statuses"
+export const ItemList = ({orderItems, setOrderItems, setItem, order, completeInspectionForOrder}) => {
     const tableHeaderData = ['Материал', 'Размер', 'Цена']
     const [isReady, setIsReady] = useState(false)
+    const {getToken} = useAuth()
+    useEffect(()=> {
+        if (!orderItems) return
+        setIsReady(orderItems.filter(item => item.isReady).length === orderItems.length)
+    }, [orderItems])
     const showItemDetails = (item) => {
         setItem(item)
     }
-    const itemCheckHandler = (e) => {
-        console.log(e.target.value)
+    const itemCheckHandler = async (item) => {
+        if (order.status !== ORDER_STATUSES.INSPECTION) return
+        const tempItem = item
+        tempItem.isReady = !item.isReady
+        await updateItemReadyState(tempItem)
+        const orderItemsTemp = orderItems.map(it => {
+            if (it.id === tempItem.id) return tempItem
+            return it
+        })
+        setOrderItems(orderItemsTemp)
+        //setIsReady(orderItemsTemp.filter(it => it.isReady).length === orderItemsTemp.length)
+        //orderItems
+        //sent item id and isReady for update and reflec this changes here if success
         //TODO
-        //There is a bug when click on check button still works select item details (devide this events)
-        //add in database isCheked property to item 
-        //check if all roderItems has checked true then set isReady to true
+        //+There is a bug when click on check button still works select item details (devide this events)
+        //+add in database isCheked property to item 
+        //
+        //+check if all roderItems has checked true then set isReady to true
+    }
+    const updateItemReadyState = async (item) => {
+        try {
+            const response = await updateItemReadyStateRequest(item.id, item.isReady, getToken())
+            console.log(response.data)
+        } catch(err) {
+            console.error(err)
+        }
+    }
+    const completeInspectionForOrderHandler = () => {
+        if (!isReady || orderItems.length === 0 || order.status !== ORDER_STATUSES.INSPECTION) return
+        completeInspectionForOrder()
     }
     return (
         <>  
@@ -32,17 +64,16 @@ export const ItemList = ({orderItems, setItem}) => {
                                 <th>{item.materialName}</th>
                                 <th>{item.size}</th>
                                 <th>{item.price}</th>
-                                <th><input style={{width:17, height: 17}} type="checkbox" 
-                                    onChange={(e)=> {itemCheckHandler(e)}}  
-                                    onClick={(e) => e.stopPropagation()}/>
+                                <th><input style={{width:17, height: 17}} type="checkbox" checked = {item.isReady} 
+                                    onClick={(e) => {e.stopPropagation(); itemCheckHandler(item)}}/>
                                 </th>
                             </tr>
                         ))}
                     </tbody>
                 </table>
-                {orderItems && <p>Итоговая цена заказа: {orderItems.reduce((acc, item) => {return acc + item.price}, 0)}₽</p>}
-                {orderItems && <p>Итоговая площадь: {orderItems.reduce((acc, item) => {return acc + item.size}, 0)}м2</p>}
-                {orderItems && <button className={(!isReady || orderItems.length === 0) && "blocked"}>Завершить проверку</button>}
+                {orderItems && <p className="lato-semibold">Итоговая цена заказа: {orderItems.reduce((acc, item) => {return acc + item.price}, 0)}₽</p>}
+                {orderItems && <p className="lato-semibold">Итоговая площадь: {orderItems.reduce((acc, item) => {return acc + item.size}, 0)}м<sup>2</sup></p>}
+                {orderItems && <button onClick={completeInspectionForOrderHandler} className={(!isReady || orderItems.length === 0 || order.status !== ORDER_STATUSES.INSPECTION) && "blocked"}>{order.status === ORDER_STATUSES.READY ? "Проверка выполнена" : "Завершить проверку"}</button>}
         </div>
             
         </>
