a4c5b0d4f699418482e045a4e28244218314be6d
lubuntum <lubuntum@gmail.com>
2025-05-10 14:17:59 +0700

    Добавлена возможность доставки заказа клиенту и его завершение со статистикой

--- src/components/order/finish-order/FinishOrderForm.jsx
+++ src/components/order/finish-order/FinishOrderForm.jsx
@@ -1,9 +1,11 @@
 import { useEffect, useState } from "react"
 import { useNavigate } from "react-router-dom"
 import { ORDER_STATUSES, STATUSES } from "../../../statuses"
-import { getOrderByIdRequest } from "../../../services/api/orderApi"
+import { changeOrderStatusRequest, getOrderByIdRequest } from "../../../services/api/orderApi"
 import { useAuth } from "../../../services/auth/AuthProvider"
 import { getItemsByOrderIdRequest } from "../../../services/api/itemApi"
+import { createOrderFinishRequest, getOrderFinishByOrderId } from "../../../services/api/orderFinishApi"
+import { formateLocalDateForServer } from "../../../services/date/dateFormattes"
 
 export const FinishOrderForm = () => {
     const [status, setStatus] = useState()
@@ -22,37 +24,79 @@ export const FinishOrderForm = () => {
         const param = new URLSearchParams(window.location.search)
         if (!param.get("id")) navigate(-1)
         const getOrderById = async () => {
-            setStatus(STATUSES.LOADING)
             const response = await getOrderByIdRequest(param.get("id"), getToken())
             console.log(response.data)
             setOrder(response.data)
+            setOrderFinishData(prev => ({...prev, orderId: response.data.id}))
         }
+        //TODO Создать отдельный обьект в orderFinishData, со статистикой заказа, и использовать ее для
+        //дополнения статистики курьера который выполнил заказ.
         const getOrderTotalPrice = async () => {
             const response = await getItemsByOrderIdRequest(param.get("id"), getToken())
-            console.log(response.data)
-            setOrder(prev => ({...prev, totalPrice: response.data.reduce((acc, item) => acc + item.price, 0)}))
+            const items = response.data
+            const totalPrice = items.reduce((acc, item) => acc + item.price, 0)
+            const itemsArea = items.reduce((acc, item) => acc + item.size, 0)
+            setOrder(prev => ({...prev, totalPrice: totalPrice}))
+            setOrderFinishData(prev => ({...prev, orderCompleteData: {totalPrice: totalPrice, itemsArea: itemsArea, workDate: formateLocalDateForServer(new Date())}}))
         }
-        try {
-            getOrderById()
-            getOrderTotalPrice()
-            setStatus(STATUSES.IDLE)
-        } catch(err) {
-            console.error(err)
-            setStatus(STATUSES.ERROR)
+        const fetchData = async() => {
+            try {
+                setStatus(STATUSES.LOADING)
+                await getOrderById()
+                await getOrderTotalPrice()
+                setStatus(STATUSES.IDLE)
+            } catch(err) {
+                console.error(err)
+                setStatus(STATUSES.ERROR)
+            }
         }
+        fetchData()
         
     }, [])
+    useEffect(()=> {
+        if (!order || order.status !== ORDER_STATUSES.COMPLETED) return
+        const getOrderFinishData = async() => {
+            const response = await getOrderFinishByOrderId(getToken(), order.id)
+            if (response.data === null) return
+            setOrderFinishData(response.data)
+        }
+        getOrderFinishData()
+    }, [order])
     const validateOrderStatus = () => {
         return order && (order.status === ORDER_STATUSES.READY || 
-            order.status === ORDER_STATUSES.COMING || 
+            order.status === ORDER_STATUSES.TAKEN || 
             order.status === ORDER_STATUSES.COMPLETED)
     }
     const isDataValid = () => {
         return (orderFinishData.itemsCount && orderFinishData.paymentMethod)
     }
+    const isOrderCompleted = () => {
+        return order.status === ORDER_STATUSES.COMPLETED
+    }
     const finishOrder = async() => {
-        if (!isDataValid()) return
-        console.log(orderFinishData)
+        if (!isDataValid() || isOrderCompleted()) return
+        try{
+            const response = await createOrderFinishRequest(getToken(), orderFinishData)
+            const statusResponse = await changeOrderStatusRequest(order.id, ORDER_STATUSES.COMPLETED, getToken())
+            setOrder(prev => ({...prev, status: statusResponse.data}))
+            setStatus(STATUSES.SUCCESS)
+            setTimeout(()=> {setStatus(STATUSES.IDLE)}, 5000)
+        } catch(err) {
+            console.error(err)
+            setStatus(STATUSES.ERROR)
+        }
+    }
+    const takeOrder = async () => {
+        if (order.status !== ORDER_STATUSES.READY) return
+        try {
+            const response = await changeOrderStatusRequest(order.id, ORDER_STATUSES.TAKEN, getToken())
+            setOrder(prev => ({...prev, status:response.data}))
+            setStatus(STATUSES.SUCCESS)
+            setTimeout(()=> {setStatus(STATUSES.IDLE)}, 5000)
+        } catch(err) {
+            console.error(err)
+            setStatus(STATUSES.ERROR)
+        }
     }
     return (
         <div className="contentWrapper">
@@ -63,6 +107,8 @@ export const FinishOrderForm = () => {
                     <div className="form">
                         <div className="formTitle">
                             <h4>Форма выдачи</h4>
+                            {status === STATUSES.ERROR && <p className="errorText">Возникла ошибка</p>}
+                            {status === STATUSES.SUCCESS && <p className="successText">Успешно</p>}
                         </div>
                         <div className="formInputs">
                             <p>ФИО: {`${order.clientName} ${order.clientSecondName} ${order.clientPatronymic}`}</p>
@@ -80,9 +126,10 @@ export const FinishOrderForm = () => {
                             <span className="custom-radio"></span>
                             Оплата наличными
                         </label>
-                        <input type="text" name="itemsCount" placeholder="Количество" onChange={handleFinishOrderData}/>
-                        <textarea name="comment" placeholder="Дополнительная информация" rows={3} onChange={handleFinishOrderData}></textarea>
-                        <button className={!isDataValid() && "blocked"} onClick={finishOrder}>Доставить заказ</button>
+                        <input type="text" name="itemsCount" placeholder="Количество" onChange={handleFinishOrderData} value={orderFinishData.itemsCount}/>
+                        <textarea name="comment" placeholder="Дополнительная информация" rows={3} onChange={handleFinishOrderData} value={orderFinishData.comment}></textarea>
+                        <button className={order.status !== ORDER_STATUSES.READY && "blocked"} onClick={takeOrder}>Забрать заказ</button>
+                        <button className={(!isDataValid() || isOrderCompleted() || order.status !== ORDER_STATUSES.TAKEN ) && "blocked"} onClick={finishOrder}>Доставить заказ</button>
                     </div>
                 </div>}
         </div>
