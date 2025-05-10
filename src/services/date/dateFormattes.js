3fa61757f42eb552abb8821b9be23a407cd0338a
lubuntum <lubuntum@gmail.com>
2025-05-10 14:16:19 +0700

    Формат даты для сервера

--- src/services/date/dateFormattes.js
+++ src/services/date/dateFormattes.js
@@ -11,3 +11,10 @@ export const formatDate = (dateString) => {
 
     return `${day}/${month}/${year} ${hours}:${minutes}`
 }
+export const formateLocalDateForServer = (date) => {
+    const year = date.getFullYear()
+    const month = String(date.getMonth() + 1).padStart(2, '0')
+    const day = String(date.getDate()).padStart(2, '0')
+
+    return `${year}-${month}-${day}`
+}
