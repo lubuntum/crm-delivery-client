b06abd0bfab685c422fabd488d3ad7210b682e36
lubuntum <lubuntum@gmail.com>
2025-07-07 06:42:53 +0700

    Новый API для получения заказов между двух дат

--- src/services/api/orderApi.js
+++ src/services/api/orderApi.js
@@ -1,5 +1,5 @@
 import axios from "axios"
-import { CHANGE_ORDER_STATUS, CREATE_ORDER, GET_ORDER_BY_ID, GET_ORDERS_TOTAL_STATS_BY_ORGANIZATION, ORGANIZATION_ORDERS, REMOVE_ORDER, SERVER_URL, UPDATE_ORDER } from "./urls"
+import { CHANGE_ORDER_STATUS, CREATE_ORDER, GET_ORDER_BY_ID, GET_ORDERS_BETWEEN_DATES, GET_ORDERS_TOTAL_STATS_BY_ORGANIZATION, ORGANIZATION_ORDERS, REMOVE_ORDER, SERVER_URL, UPDATE_ORDER } from "./urls"
 
 export const createOrderRequest = async (order, token) => {
     return await axios.post(`${SERVER_URL}${CREATE_ORDER}`, order, {
@@ -33,6 +33,11 @@ export const getOrderByIdRequest = async (id, token) => {
 }
 export const getOrdersTotalStatsByOrganizationRequest = async (token) => {
     return await axios.get(`${SERVER_URL}${GET_ORDERS_TOTAL_STATS_BY_ORGANIZATION}`, {
+        headers: {Authorization: token}
+    })
+}
+export const getOrdersBetweenDates = async (startDate, endDate, token) => {
+    return axios.get(`${SERVER_URL}${GET_ORDERS_BETWEEN_DATES}?startDate=${startDate}&endDate=${endDate}`, {
         headers: {Authorization: token}
     })
 }
