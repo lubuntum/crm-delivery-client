b47df85bbcc1cd2c9d8c7dce039d5cbeb0716f93
lubuntum <lubuntum@gmail.com>
2025-07-31 08:45:26 +0700

    api для работы админа по заказам

--- src/services/api/adminApi.js
+++ src/services/api/adminApi.js
@@ -1,5 +1,5 @@
 import axios from "axios"
-import { ADD_ORGANIZATION, CHANGE_ORGANIZATION_ACTIVE_STATUS, GET_ORGANIZATIONS, SERVER_URL } from "./urls"
+import { ADD_ORGANIZATION, CHANGE_ORGANIZATION_ACTIVE_STATUS, GET_ORDERS_BETWEEN_DATES_BY_ORGANIZATION, GET_ORGANIZATIONS, SERVER_URL } from "./urls"
 
 export const getOrganizationsRequest = async (token) => {
     return await axios.get(`${SERVER_URL}${GET_ORGANIZATIONS}`, {
@@ -13,6 +13,11 @@ export const changeOrganizationActiveStatusRequest = async (token, organization)
 }
 export const addOrganizationRequest = async (token, organization) => {
     return await axios.post(`${SERVER_URL}${ADD_ORGANIZATION}`, organization, {
+        headers: {Authorization: token}
+    })
+}
+export const getOrdersBetweenDatesByOrganization = async (token, startDate, endDate, organizationId) => {
+    return await axios.get(`${SERVER_URL}${GET_ORDERS_BETWEEN_DATES_BY_ORGANIZATION}?startDate=${startDate}&endDate=${endDate}&organizationId=${organizationId}`, {
         headers: {Authorization: token}
     })
 }
