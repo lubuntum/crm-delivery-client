dd3f8e22db38f18aa4769c165e445d7248669a9d
lubuntum <lubuntum@gmail.com>
2025-08-15 18:24:12 +0700

    Доработана главняа страница

--- src/components/home/HomePage.jsx
+++ src/components/home/HomePage.jsx
@@ -2,12 +2,19 @@ import "./css/home_style.css"
 
 import { useAuth } from "../../services/auth/AuthProvider"
 import { RecentNews } from "./RecentNews"
+import { useNavigate } from "react-router-dom"
+import { ROUTES } from "../../routes"
 
 export const HomePage = () => {
     const { checkAuth } = useAuth()
+    const navigate = useNavigate()
     if (!checkAuth()) return( 
         <div className="contentWrapper">
-            <p>Авторизуйтесь для работы</p>
+            <div className="homeWrapper">
+                <p className="loginTitle">Хотите автоматизировать бизнес процессы ?</p>
+                <button className="customButton" onClick={() => navigate(ROUTES.ORGANIZATION_REQUEST)}>Регистрация организации</button>
+                <button className="customButton" onClick={() => navigate(ROUTES.AUTH)}>Начать</button>
+            </div>
         </div>)
     return (
         <div className="contentWrapper" style={{alignItems: "end"}}>
