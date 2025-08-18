7249ad02eb85879b73c76c087a304602179b4ec6
lubuntum <lubuntum@gmail.com>
2025-08-18 07:01:03 +0700

    API для получение уведомлений по статусу

--- src/services/api/organizationRequest.js
+++ src/services/api/organizationRequest.js
@@ -1,5 +1,5 @@
 import axios from "axios"
-import { CREATE_ORG_REGISTRATION_REQUEST, SERVER_URL, UPDATE_ORG_REGISTRATION_REQUEST_STATUS } from "./urls"
+import { CREATE_ORG_REGISTRATION_REQUEST, GET_ORG_REGISTRATION_REQUESTS_BY_STATUS, SERVER_URL, UPDATE_ORG_REGISTRATION_REQUEST_STATUS } from "./urls"
 
 export const createOrganizationRequest = async (organizationRequest) => {
     return await axios.post(`${SERVER_URL}${CREATE_ORG_REGISTRATION_REQUEST}`, organizationRequest)
@@ -7,5 +7,10 @@ export const createOrganizationRequest = async (organizationRequest) => {
 export const updateOrganizationRequestStatus = async (organizationRequest, token) => {
     return await axios.post(`${SERVER_URL}${UPDATE_ORG_REGISTRATION_REQUEST_STATUS}`, organizationRequest, {
         headers: {Authorization: token}
+    })
+}
+export const getOrganizationRequestsByStatus = async (status, token) => {
+    return await axios.get(`${SERVER_URL}${GET_ORG_REGISTRATION_REQUESTS_BY_STATUS}?status=${status}`, {
+        headers:{Authorization: token}
     })
 }
