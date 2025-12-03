49d78eef1e86ec64248148f2f758decccf9f31ab
lubuntum <lubuntum@gmail.com>
2025-12-03 10:26:20 +0700

    Исправлен баг когда заказы не открывались оффлайн, проблемы с кэшированием роута orders были

--- public/sw.js
+++ public/sw.js
@@ -1,76 +1,40 @@
 const CACHE_NAME = "crm-delivery-offline-v1"
-const urlsToCache = [
+
+const STATIC_URLS = [
     '/',
     '/home',
     '/static/js/bundle.js',
     '/static/css/main.css',
     '/manifest.json',
-    '/orders'
 ]
 
 self.addEventListener('install', (event) => {
     event.waitUntil(
         caches.open(CACHE_NAME)
             .then((cache) => {
-                return Promise.all(
-                    urlsToCache.map(url => {
-                        return cache.add(url).catch(err => {
-                            console.error(`Failed to cache ${url}`, err)
-                        })
-                    })
-                )
+                return cache.addAll(STATIC_URLS)
+                    .catch(err => {console.error("Failed to cache some static assets", err)})
             })
     )
     self.skipWaiting()
 })
 
 self.addEventListener('fetch', event => {
-    if (event.request.method !== 'GET') return
+    const {request} = event
+    const url = new URL (request.url)
+    if (request.url.includes("/api/") || request.method !== "GET" ||
+        (request.headers.get("Accept") && request.headers.get("Accept").includes("application/json"))) 
+        return
     
     event.respondWith(
-        caches.match(event.request)
-            .then(response => {
-                if (response) return response
-                
-                return fetch(event.request)
-                    .then(response => {
-                        if (!response || response.status !== 200 || response.type !== 'basic') {
-                            return response
-                        }
-                        
-                        const responseToCache = response.clone()
-                        caches.open(CACHE_NAME)
-                            .then(cache => {
-                                cache.put(event.request, responseToCache)
-                            })
-                        return response
-                    })
-                    .catch(err => {
-                        console.error("Network failed, serving from cache ", err)
-                        return caches.match('/') || new Response("Offline", {
-                            status: 503, 
-                            statusText: "Service Unavailable",
-                            headers: new Headers({
-                                "Content-Type": "text/plain"
-                            })
-                        })
-                    })
+        fetch(request)
+            .catch(() => {
+                if (event.request.mode === "navigate") return caches.match('/')
+                return caches.match(request)
             })
     )
 })
 
 self.addEventListener('activate', (event) => {
-    event.waitUntil(
-        caches.keys().then((cacheNames) => {
-            return Promise.all(
-                cacheNames.map(cacheName => {
-                    if (cacheName !== CACHE_NAME) { 
-                        return caches.delete(cacheName)
-                    }
-                })
-            )
-        }).then(() => {
-            return self.clients.claim()
-        })
-    )
+    event.waitUntil(self.clients.claim())
 })
